33fc51394232fc4b4ae3c217f2d677ba02caef8b
parkjunsun <qkrwnstns52@naver.com>
2022-05-07 15:26:44 +0900

    2022.05.06 AOP 실전 예제

--- src/test/java/hello/aop/pointcut/ThisTargetTest.java
+++ src/test/java/hello/aop/pointcut/ThisTargetTest.java
@@ -6,7 +6,6 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
